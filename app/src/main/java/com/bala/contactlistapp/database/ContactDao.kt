3b8dd3cc702bf9e1e756f9cb067e478b39203dde
Balamurugan <balamurugan.wefour@gmail.com>
2021-09-06 01:01:11 +0530

    Initial Commit by Bala

--- app/src/main/java/com/bala/contactlistapp/database/ContactDao.kt
+++ app/src/main/java/com/bala/contactlistapp/database/ContactDao.kt
@@ -17,8 +17,6 @@ import io.reactivex.Single
 
 @Dao
 open interface ContactDao {
-    @Insert
-    fun insertUserData(contactDataBaseModel: ContactDataBaseModel?): Single<Long?>?
 
     @Query("SELECT * FROM contactDataBaseModel")
     fun getAllList(): Single<List<ContactDataBaseModel>>
@@ -26,22 +24,12 @@ open interface ContactDao {
     @Query("UPDATE contactDataBaseModel SET name =:name,phone =:phone,email =:email,photo =:photo WHERE dbid =:id")
     fun updateData(name: String?, phone: String?, email: String?, photo: String?, id: Int): Int
 
-  /*  @Query("SELECT * FROM contactDataBaseModel WHERE dbid=:idData")
-    fun getIdByResponseData(idData: Int)
-
-    @Query("SELECT * FROM contactdatabasemodel WHERE dbid=:idData")
-    fun checkDataPresent(idData: Int)*/
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertData(responseBaseClass: ContactDataBaseModel?): Long?
 
     @Query(" DELETE FROM contactDataBaseModel")
     fun deleteDataBase(): Completable
 
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertPersonDataList(dataList: List<ContactDataBaseModel>): Completable
-
 
 
 }
